09236d96a2238f4dfd24bad0c1539ba24ad5c32d
nbur4556 <nbur4556@gmail.com>
2021-04-21 21:18:34 -0500

    feat: create list component and which lists all posts in state

--- packages/my-first-theme/src/components/index.js
+++ packages/my-first-theme/src/components/index.js
@@ -1,8 +1,13 @@
 import React from 'react';
 import { connect } from 'frontity';
 import Link from '@frontity/components/link';
+import Switch from '@frontity/components/switch';
+
+import List from './list';
 
 const Root = ({ state }) => {
+    const data = state.source.get(state.router.link);
+
     return (
         <>
             <h1>Hello Frontity</h1>
@@ -14,6 +19,14 @@ const Root = ({ state }) => {
                 <Link link="/travel-tips">Travel Tips</Link>
                 <Link link="/about">About Me</Link>
             </nav>
+
+            <main>
+                <Switch>
+                    <List when={data.isArchive} />
+                    <div when={data.isPost}>This is a post</div>
+                    <div when={data.isPage}>This is a page</div>
+                </Switch>
+            </main>
         </>
     )
 }
