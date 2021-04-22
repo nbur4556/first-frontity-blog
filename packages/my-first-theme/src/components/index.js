b0e4956af171b0d1a1b04c58cd839f1fb5915e86
nbur4556 <nbur4556@gmail.com>
2021-04-21 20:43:59 -0500

    feat: create links to blog routes

--- packages/my-first-theme/src/components/index.js
+++ packages/my-first-theme/src/components/index.js
@@ -1,11 +1,19 @@
 import React from 'react';
 import { connect } from 'frontity';
+import Link from '@frontity/components/link';
 
 const Root = ({ state }) => {
     return (
         <>
             <h1>Hello Frontity</h1>
             <p>Current URL: {state.router.link}</p>
+
+            <nav>
+                <Link link="/">Home</Link>
+                <Link link="/destinations">Destinations</Link>
+                <Link link="/travel-tips">Travel Tips</Link>
+                <Link link="/about">About Me</Link>
+            </nav>
         </>
     )
 }
